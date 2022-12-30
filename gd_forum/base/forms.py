63d05f55ac04214f6e4e02495098f257b5f31888
Luka Filipić <bubific@gmail.com>
2022-12-30 23:10:49 +0100

    Form dizajn renoviran i novi dizajn za thread form

--- gd_forum/base/forms.py
+++ gd_forum/base/forms.py
@@ -10,6 +10,20 @@ class ThreadForm(ModelForm):
     class Meta:
         model = Thread
         fields = '__all__'
+    def __init__(self, *args, **kwargs):
+        super(ThreadForm, self).__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs['class'] = 'form-control'
+        self.fields['title'].widget.attrs['id'] = 'floatingInput'
+        self.fields['title'].widget.attrs['placeholder'] = 'title'
+        self.fields['description'].widget.attrs['class'] = 'form-control textarea'
+        self.fields['description'].widget.attrs['placeholder'] = 'description'
+        self.fields['description'].widget.attrs['cols'] = '8'
+        self.fields['description'].widget.attrs['id'] = 'floatingInput'
+        self.fields['uploader'].widget.attrs['class'] = 'form-select'
+        self.fields['uploader'].widget.attrs['id'] = 'floatingInput'
+        self.fields['category'].widget.attrs['class'] = 'form-select'
+        self.fields['category'].widget.attrs['id'] = 'floatingInput'
         
 class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'e-mail'}))
@@ -22,7 +36,10 @@ class RegisterUserForm(UserCreationForm):
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'username'
+        self.fields['username'].widget.attrs['id'] = 'floatingInput'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'password'
+        self.fields['password1'].widget.attrs['id'] = 'floatingInput'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'password confirmation'
+        self.fields['password2'].widget.attrs['id'] = 'floatingInput'
